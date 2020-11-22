154f79cb808e5bbdad0231485ead0407791ded24
Brayan Potosi <bp990308@hotmail.com>
2020-11-22 17:59:56 -0500

    DEBUG true

--- tienda/settings.py
+++ tienda/settings.py
@@ -143,6 +143,8 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # EMAIL_HOST = "smtp.gmail.com"
 # EMAIL_USE_TLS = True
